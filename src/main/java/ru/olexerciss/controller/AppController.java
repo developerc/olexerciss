17f35763b8a8eae32f5514b19f0bdc2c9668d298
bliz <developerc@yandex.ru>
2018-06-14 16:51:05 +0300

    add index3

--- src/main/java/ru/olexerciss/controller/AppController.java
+++ src/main/java/ru/olexerciss/controller/AppController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AppController {
     @RequestMapping("/")
     public String helloPage(){
-        return "index2";
+        return "index3";
     }
 }
